3abbc464a019aa33a4bc6bd8381e1d001d38b431
Алексей Дорошенко <spartacity@mail.ru>
2020-09-19 22:06:41 +0300

    [+] added about/left/right/action

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -11,12 +13,22 @@ class Creators(Screen):
     pass
 
 
+class About(Screen):
+    pass
+
+
+class StartGame(Screen):
+    pass
+
+
 class MainApp(App):
     def build(self):
         # создаем менеджер экранов
         scren_manager = ScreenManager()
         scren_manager.add_widget(MainMenu(name="menu"))
         scren_manager.add_widget(Creators(name="creators"))
+        scren_manager.add_widget(About(name="about"))
+        scren_manager.add_widget(StartGame(name="start_game"))
         return scren_manager
 
 
