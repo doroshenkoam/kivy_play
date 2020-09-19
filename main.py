b72d7da4e3c5f61dfdfdf1591283dfbcc8fb2a67
Алексей Дорошенко <spartacity@mail.ru>
2020-09-19 03:05:47 +0300

    [+] added menu/creators

---
+++ main.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class MainMenu(Screen):
+    pass
+
+
+class Creators(Screen):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        # создаем менеджер экранов
+        scren_manager = ScreenManager()
+        scren_manager.add_widget(MainMenu(name="menu"))
+        scren_manager.add_widget(Creators(name="creators"))
+        return scren_manager
+
+
+if __name__ == "__main__":
+    MainApp().run()
